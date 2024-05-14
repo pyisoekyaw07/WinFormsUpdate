4b74e1c5fcb20b78a51f0f20a83d8759f90a0fcb
pyisoekyaw <satansatan755@gmail.com>
2024-05-14 12:43:36 -0700

    update commit

--- GHF/GHF/obj/Release/net6.0-windows/GHF.AssemblyInfo.cs
+++ GHF/GHF/obj/Release/net6.0-windows/GHF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GHF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0497ba8506823aa7bb20a08fe8c4d77dd5b56ac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eadcf921c5fa92fb2d6d37761f4f86d03872db20")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
